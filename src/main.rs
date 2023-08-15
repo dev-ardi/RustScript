ad16123d9e3856ce7da3b8e0bc297f18285ec2d2
orion GONZALEZ (contractor) <orion.gonzalez@amadeus.com>
2023-08-15 16:33:30 +0200

    better handling of user input

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,30 @@ enum Token<'a> {}
 #[derive(Debug, Clone, Copy, Hash)]
 enum TokenOwning {}
 
-fn lexer<'a>(source: &'a str) -> impl Iterator<Item = Token<'a>> {}
+fn lexer<'a>(source: &'a str) -> anyhow::Result<impl Iterator<Item = Token<'a>>> {}
 
 fn main() -> anyhow::Result<()> {
     match env::args().nth(2) {
+        Some(&"-h") | Some(&"--help") => {
+            println!("Welcome to RustScript. You can either run a source file with rsc path/to/file or just type rsc to enter the REPL");
+            Ok(())
+        }
         Some(source_file) => {
             let source = std::io::read_to_string(source_file)?;
+            let lexed = lexer(source.as_str());
 
-            for line in source.lines() {
-                run(line)?;
-            }
-            return Ok(());
+            Ok(())
         }
+        // REPL
         None => {
             let mut line = String::new();
             loop {
                 print!("> ");
-                io::stdin().read_line(&mut line)?;
+
+                if let Ok(0) = io::stdin().read_line(&mut line)? {
+                    Ok(()) // Control D
+                }
+
                 if let Err(e) = run(line.as_str()) {
                     println!("{:?}", e)
                 }
